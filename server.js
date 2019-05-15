5074494bbb090c6038e06879d69f0988fb336884
raberin <rmvaberin@gmail.com>
2019-05-14 19:32:35 -0700

    First Commit - Added index.js code to listen for at port 4000

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = 'express';
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  console.log(`[${req.method}] Request - `);
+  next();
+}
 
 module.exports = server;
