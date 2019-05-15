c91df27a3b37bd4e2129e4596af52e56a18a6e12
raberin <rmvaberin@gmail.com>
2019-05-14 21:36:01 -0700

    Added validatePostId

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,43 @@
 const express = 'express';
 
+const postDb = require('./postDb.js')
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, (req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
 
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const post = await  postDb.getById(id);
+    if(post) {
+      //By setting req.post with the above async, the value is being passed down 
+      //To next async function. 
+      req.post = post;
+      next();
+    } else {
+      res.status(400).json({ message: 'invalid post id'})
+    }
 
+  } catch (err) {
+    next({ message: "Failed to process request"})
+  }
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,27 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
+//custom middleware
+server.use(logger);
+
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
 function logger(req, res, next) {
-  console.log(`[${req.method}] Request - `);
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} Request - ${req.url} `
+  );
   next();
 }
+
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "Err err errror",
+    err
+  });
+});
 
 module.exports = server;
