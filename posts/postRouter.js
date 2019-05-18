4f62551a04cac5e6a11dc67416c95de23ca8b5ff
raberin <rmvaberin@gmail.com>
2019-05-18 13:52:09 -0700

    MVP Complete, Added get request in postRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,43 +1,43 @@
-const express = 'express';
+const express = require("express");
 
-const postDb = require('./postDb.js')
+const postDb = require("./postDb.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", async (req, res) => {
+  try {
+    const posts = await postDb.get();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({
+      message: "Could not grab post data"
+    });
+  }
 });
 
-router.get('/:id', validatePostId, (req, res) => {
+router.get("/:id", validatePostId, (req, res) => {});
 
-});
+router.delete("/:id", validatePostId, (req, res) => {});
 
-router.delete('/:id', validatePostId, (req, res) => {
-
-});
-
-router.put('/:id', validatePostId, (req, res) => {
-
-});
+router.put("/:id", validatePostId, (req, res) => {});
 
 // custom middleware
 
-function validatePostId(req, res, next) {
+async function validatePostId(req, res, next) {
   try {
     const { id } = req.params;
-    const post = await  postDb.getById(id);
-    if(post) {
-      //By setting req.post with the above async, the value is being passed down 
-      //To next async function. 
+    const post = await postDb.getById(id);
+    if (post) {
+      //By setting req.post with the above async, the value is being passed down
+      //To next async function.
       req.post = post;
       next();
     } else {
-      res.status(400).json({ message: 'invalid post id'})
+      res.status(400).json({ message: "invalid post id" });
     }
-
   } catch (err) {
-    next({ message: "Failed to process request"})
+    next({ message: "Failed to process request" });
   }
-};
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const userRouter = require("./users/userRouter.js");
+const postRouter = require("./posts/postRouter.js");
 
 const server = express();
 
@@ -11,6 +12,7 @@ server.use(logger);
 
 //routers
 server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
